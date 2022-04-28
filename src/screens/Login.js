7d63d6ab1dfac6c8df7c9af0c94a5b3786797c35
Mustodik <dawbiemusdoiq30@gmail.com>
2022-04-28 15:28:28 +0000

    added new data

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,21 +1,46 @@
-import { Link } from 'react-router-dom'
-import React from 'react'
+import React,{useState, useEffect, useContext} from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import {AuthContext} from "../contexts/auth.Context"
+
+
 
 function Login() {
+  const navigate = useNavigate();
+  const {success, loading, user, authLogin} = useContext(AuthContext);
+  const [email, setEmail,] = useState("");
+  const [password,setPassword] = useState("");
+
+  useEffect(() => {
+    if (success) {
+      navigate("/home");
+    }
+  }, [success]);
+  const handleSubmit = (e) => {
+    e.preventDefult();
+    authLogin(email, password)
+  };
   return (
-      <form className='form'>
+      <form onSubmit={handleSubmit} className='form'>
       <div className='col-sm-12 col-md-6 col-lg-4 col-xl-4 m-3 p-3 bg-info'>
     <div className='form-group m-2'>
     <label htmlFor='email'>email</label>
-    <input type='text' className='form-control' placeholder='Enter email'/>
+    <input type='text' className='form-control' placeholder='Enter email'
+    value={email}
+    onChange={(e) => setEmail(e.target.value)}
+    />
     </div>
 
     <div className='form-group m-2'>
     <label htmlFor='password'>password</label>
-    <input type='password' className='form-control' placeholder='Enter password'/>
+    <input type='password' 
+    className='form-control' 
+    placeholder='Enter password'
+    value={password}
+    onChange={(e) => setPassword(e.target.value)}
+    />
     </div>
-    <button type='submit' className='btn btn-primary'>
-      Login
+    <button type='submit' className='btn btn-primary m-2'>
+      {loading ? "loading...." : "Login"}
     </button>
     <div>
     i don't have an account
